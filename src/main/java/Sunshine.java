9e425ec04b8da3c3b75c2942413c5258b169d8f0
Justin Lee <justinleetw@gmail.com>
2026-01-21 22:33:44 +0800

    Add support for echo

--- src/main/java/Sunshine.java
+++ src/main/java/Sunshine.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+
 public class Sunshine {
     public static void main(String[] args) {
-        String msg = "____________________________________________________________\n"
-                + "Hello! I'm Sunshine\n"
-                + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
+        String welcome = "    ____________________________________________________________\n" +
+                "    Hello! I'm Sunshine\n" +
+                "    What can I do for you?\n" +
+                "    ____________________________________________________________\n";
 
-        System.out.println(msg);
+        System.out.println(welcome);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String cmd = scanner.nextLine();
+            if (cmd.equals("bye")) {
+                break;
+            }
+            System.out.println("    ____________________________________________________________\n" +
+                    "    " + cmd + "\n" +
+                    "    ____________________________________________________________\n");
+        }
+
+        String farewell = "    ____________________________________________________________\n" +
+                "     Bye. Hope to see you again soon!\n" +
+                "    ____________________________________________________________\n";
+        System.out.println(farewell);
     }
 }
