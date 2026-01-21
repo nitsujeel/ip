87647c33fde8007a78259f88d5821d5996077718
Justin Lee <justinleetw@gmail.com>
2026-01-21 22:06:00 +0800

    Implement greet message

--- src/main/java/Sunshine.java
+++ src/main/java/Sunshine.java
@@ -1,10 +1,12 @@
 public class Sunshine {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String msg = "____________________________________________________________\n"
+                + "Hello! I'm Sunshine\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+
+        System.out.println(msg);
     }
 }
